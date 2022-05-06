896c726684da8810de8ef20ee216cbc408a64628
sanjay-tringapps <96760013+sanjay-tringapps@users.noreply.github.com>
2022-05-06 16:49:47 +0530

    Update reducer.js

--- redux/reduxlayout/src/reducer.js
+++ redux/reduxlayout/src/reducer.js
@@ -5,13 +5,12 @@ const appReducer=(state,action)=>{
   const inter=[action.task,action.task,initialState[2],initialState[3],initialState[4]]
   
    switch(action.type){
-       case 'ADD_TASK':
-        return inter;
 
-           
-           default:
-               
-               return initialState;
+       case 'ADD_TASK':
+          return inter;  
+
+          default:     
+            return initialState;
    }
    
 };
