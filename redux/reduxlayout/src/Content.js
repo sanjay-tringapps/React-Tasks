78d52f5ef3570d30179852e6463374c578095b83
sanjay-tringapps <96760013+sanjay-tringapps@users.noreply.github.com>
2022-05-06 16:43:50 +0530

    Update Content.js

--- redux/reduxlayout/src/Content.js
+++ redux/reduxlayout/src/Content.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {connect,useDispatch, useSelector} from 'react-redux'
+import {connect, useSelector} from 'react-redux'
 import {addTask} from './actions'
 
 function Content({addNewTask}) {
